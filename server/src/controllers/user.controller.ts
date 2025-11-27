7995c053af55a3559a214d0e46a9b04d7e846325
morganelepine <m.lepineutter@gmail.com>
2025-11-27 16:38:03 +0100

    Check that username is not already used before creating user

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -28,15 +28,23 @@ export class UserController {
     // POST /users
     async save(request: Request, response: Response, next: NextFunction) {
         const { uuid, score } = request.body;
-        let username;
 
-        if (usernames.length === 0) {
-            username = "username";
+        const usedUsers = await prisma.user.findMany({
+            select: { username: true }
+        });
+        const usedUsernames = usedUsers.map(u => u.username);
+        console.log(usedUsernames);
+
+        const availableUsernames = usernames.filter(name => !usedUsernames.includes(name));
+        console.log(availableUsernames);
+
+        if (availableUsernames.length === 0) {
+            return { status: 404, message: "No username available" };
         }
 
-        const randomIndex = Math.floor(Math.random() * usernames.length);
-        username = usernames[randomIndex];
-        usernames.splice(randomIndex, 1);
+        const randomIndex = Math.floor(Math.random() * availableUsernames.length);
+        const username = availableUsernames[randomIndex];
+        console.log(username);
 
         const user = await prisma.user.create({
             data: { uuid, username, score },
